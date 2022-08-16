23a85ba950252bcf48887c9862fbb6797c950b7f
rodrigo.lavratti <rodrigo.lavratti@compasso.com.br>
2022-08-15 21:39:59 -0300

    Criação da pág. Home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './component/pages/Home';
+import Company from './component/pages/Company';
+import Contact from './component/pages/Contact';
+import NewProject from './component/pages/NewProject';
+import Projects from './component/pages/Projects';
+
+import Container from './component/layout/Container';
+import Navbar from './component/layout/Navbar';
+import Footer from './component/layout/Footer';
 
 
 function App() {
   return (
     <div className="App">
-      <p>Consts</p>
+      <Router>
+        <Navbar />
+        <Container customClass="min-height">
+          <Routes>
+            <Route exact path='/' element={<Home />}></Route>
+            <Route exact path='/projects' element={<Projects />}></Route>
+            <Route exact path='/company' element={<Company />}></Route>
+            <Route exact path='/contact' element={<Contact />}></Route>
+            <Route exact path='/newproject' element={<NewProject />}></Route>
+          </Routes>
+        </Container>
+        <Footer />
+      </Router>
     </div>
   );
 }
---
+++ src/component/pages/NewProject.js
@@ -0,0 +1,9 @@
+function NewProject() {
+    return (
+        <div>
+            <h1>Criar Projeto</h1>
+            <p>Crie seu projeto para depois adicionar os serviços</p>
+        </div>
+    )
+}
+export default NewProject
